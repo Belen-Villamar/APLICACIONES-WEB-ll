d75b032252794bfc1b645e4a72b872a9f58dd8ae
Belén Villamar Alcívar <92699492+Belen-Villamar@users.noreply.github.com>
2022-07-06 16:04:28 -0500

    main

--- Complementarios en Clases/14CSR/vite-project/README.md
+++ Complementarios en Clases/14CSR/vite-project/README.md
@@ -3,3 +3,7 @@ npm create vite@latest
 TENER EN CUENTA UBICARSE EN EJECUTARSE DONDE SE CREO EL NUEVO PROYECTO
 npm install
 npm run dev
+
+Se cambió el puerto al servicio rest a 2500 para no tener conflicto con el puerto 3000 del localhost de este nuevo proyecto.
+
+Se descargo una extension llamada Paste JSON as Code para realizar la interfaces de la tabla como tal
--- Complementarios en Clases/14CSR/vite-project/src/main.ts
+++ Complementarios en Clases/14CSR/vite-project/src/main.ts
@@ -24,7 +24,7 @@ etiqueta.htmlFor="id"
 
 app.appendChild(etiqueta);
 app.appendChild(input)
-
+//Cuerpo de estrucutura para el fronde
 app.innerHTML += `
 <label for 'nombre'>Nombre</label><input id='nombre' />
 <label for 'estado'>Estado</label><input id='estado' />
@@ -53,6 +53,7 @@ const consultar = document.querySelector<HTMLButtonElement>("#consultar")!
 
 const cuerpo = document.querySelector<HTMLDivElement>('#cuerpo')!
 
+//Funcion para limpiar si hay datos ingresados.
 nuevo.addEventListener('click', ()=>{
   id.value =""
   nombre.value =""
@@ -62,37 +63,70 @@ nuevo.addEventListener('click', ()=>{
   stock.value =""
   minimo.value =""
 })
-
+//Funcion para consultar un producto en especifico
 consultar.addEventListener('click', async ()=>{
   const resproductos:Extension = await( await httpAxios.get<Extension>('productos')).data
   console.log(resproductos);
   const {productos} = resproductos
-
+//Funcion para crear una tabla
   const tabla = document.createElement('table');
   tabla.id="tabla"
   tabla.border="1"
 
-  for (const producto of productos)
-  {
+  for(const producto of productos){
     const row = tabla.insertRow();
     const celda = row.insertCell();
-    celda.innerHTML= `<button class="boton" values '${producto.nombre}'> ${producto.nombre} </button>` 
-    const celda2= row.insertCell();
-    celda2.innerHTML= `${producto.precio}`
-
-   
+//Boton para selecionar un producto y realizar una consulta del producto
+    celda.innerHTML=`<button class="boton" value='${producto._id}'>${producto.nombre}</button>`;
+    const celda2 = row.insertCell();
+    celda2.innerHTML=`${producto.precio}`
   }
   cuerpo.innerHTML=""
   cuerpo.appendChild(tabla)
 
-  document.querySelectorAll('.boton').forEach((ele:Element) =>{
-    (ele as HTMLButtonElement).addEventListener('click', ()=>
-    {
-      httpAxios.get(`productos/62ba0ef1ab25bf0e2a052c59`)
+//Capturo los datos de un producto
+  document.querySelectorAll('.boton').forEach((ele:Element)=>{
+    ele.addEventListener('click',async()=>{
+      const {data} = await httpAxios.get<Producto>(`productos/${(ele as HTMLButtonElement ). value}`);
+      console.log(data);
+      nombre.value= data.nombre;
+      precio.value= data.precio.toString();
+      costo.value= data.costo.toString();
+      minimo.value= data.minimo.toString();
+      stock.value= data.stock.toString();
+      estado.value= data.estado!.toString();
+      id.value = data._id!
 
-      
     })
-    
   })
 })
-
+//Funcion para asginar cada valor
+const asignarValores=()=>{
+  const data:Producto ={
+    nombre: nombre.value,
+    costo: Number (costo.value),
+    precio: Number (precio.value),
+    minimo: Number (minimo.value),
+    stock: Number (stock.value),
+  }
+  return data;
+}
+//Metodo para grabar datos modificar y guardar
+grabar.addEventListener('click',async ()=>{
+  const data = asignarValores()
+  if(id.value.trim().length>0)
+  {
+    const respproductos:Producto = await (await httpAxios.put<Producto>(`productos/${id.value}`, data)).data
+    console.log(`El producto ${respproductos.nombre} fue modificado con éxito`);
+    return;
+  }
+  try {
+    const respproductos: Producto = await (await httpAxios.post <Producto>(`productos`, data)).data
+    console.log(`El producto ${respproductos.nombre} fue insertado con éxito`);
+  } catch (error) {
+    if (axios.isAxiosError(error)){
+      console.log(`Error en axios`);
+    }
+    console.log(error);
+  }
+})
